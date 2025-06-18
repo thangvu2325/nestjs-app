0de78c7947a1ecab07f8011548e29990386978b6
Vũ Đức Thắng <99258025+thangvu2325@users.noreply.github.com>
2025-06-18 21:49:12 +0700

    Update mqttOptions.ts

--- src/mqtt/mqttOptions.ts
+++ src/mqtt/mqttOptions.ts
@@ -1,9 +1,10 @@
+import { MqttProtocol } from 'mqtt/*';
 import { MqttModuleOptions } from './mqtt.interface';
 
 export const mqttOptions: MqttModuleOptions = {
   hostname: process.env.MQTT_HOSTNAME,
   port: Number(process.env.MQTT_PORT),
-  protocol: 'mqtts', // Explicitly specify TLS
+  protocol: process.env.MQTT_PROTOCOL as MqttProtocol || "mqtts", // Explicitly specify TLS
   keepalive: 60, // Reduced to a standard value (in seconds)
   reconnectPeriod: 5000, // Increased to avoid aggressive reconnections (in ms)
   username: process.env.MQTT_USERNAME || 'admin2', // Use environment variable
