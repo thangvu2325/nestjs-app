ab176360e435cb45a5696e4199b3476f4383c104
Vũ Đức Thắng <99258025+thangvu2325@users.noreply.github.com>
2025-06-19 11:23:32 +0700

    Update mqttOptions.ts

--- src/mqtt/mqttOptions.ts
+++ src/mqtt/mqttOptions.ts
@@ -3,12 +3,13 @@ import { MqttModuleOptions } from './mqtt.interface';
 
 export const mqttOptions: MqttModuleOptions = {
   hostname: process.env.MQTT_HOSTNAME,
-  port: Number(process.env.MQTT_PORT),
-  protocol: process.env.MQTT_PROTOCOL as MqttProtocol || "mqtts", // Explicitly specify TLS
-  keepalive: 60, // Reduced to a standard value (in seconds)
-  reconnectPeriod: 5000, // Increased to avoid aggressive reconnections (in ms)
-  username: process.env.MQTT_USERNAME || 'admin2', // Use environment variable
-  password: process.env.MQTT_PASSWORD || 'Thang123456', // Use environment variable
-  protocolVersion: 5, // MQTT 5.0, assuming broker support
-  clean: true, // Clean session, adjust if persistent session is needed
+  port: Number(process.env.MQTT_PORT) || 8884,
+  protocol: process.env.MQTT_PROTOCOL as MqttProtocol,
+  path: '/mqtt', 
+  keepalive: 60,
+  reconnectPeriod: 5000,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  protocolVersion: 5,
+  clean: true,
 };
